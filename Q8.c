d74e22cb11b7c89f2e5765f9f2da73e8ad0d2371
Ahmad30Jalal <115393713+Ahmad30Jalal@users.noreply.github.com>
2024-11-17 22:12:53 +0500

    Update Q8.c

--- Q8.c
+++ Q8.c
@@ -2,7 +2,9 @@
 #include <string.h>
 #include <math.h>
 
-void btd(int arr[], int n) {
+void btd() {
+int arr[20];
+int n;    
 
 int s = 0, sum = 0;
 int i, n1;
@@ -10,9 +12,15 @@ int i, n1;
 printf("Enter number of bits: ");
 scanf("%d", &n);
 
+
+
 for (i = 0; i < n; i++) {
     printf("Bit %d: ", i + 1);
     scanf("%d", &arr[i]);
+    if (arr[i] < 0 || arr[i] > 1) {
+        printf("Enter a valid value\n");
+        i--;
+    }
     
 }
 
@@ -27,36 +35,166 @@ for (i = 0; i < n; i++) {
   
 }
 
-void dtb(int n) {
-int i, j, n1, n
-int sum = 0;
+void dtb() {
+    int n, i = 0, arr[32]; 
+    printf("Enter a decimal number: ");
+    scanf("%d", &n);
 
-while (n > 0) {
-  n = n % 2;
+    while (n > 0) {
+        arr[i] = n % 2; 
+        n /= 2;         
+        i++;
+    }
+
+    printf("Decimal to binary: ");
+    for (int j = i - 1; j >= 0; j--) {  
+        printf("%d", arr[j]);
+    }
+    printf("\n");
+}
+
+void htd() {
+char val[20];
+int i, n2, n1;
+int ans = 0;
+
+printf("Enter Hexadecimal number: ");
+scanf("%s", val);
+    
+  n2 = strlen(val);
   
-  if (n % 2 == 0) {
-    i = 0;
-    n1 = arr{i];
+    
+    for (i = 0; i < n2; i++) {
+        char n3 = val[i];
+        
+        if (n3 >= '0' && n3 <= '9') {
+            n1 = n3 - '0';
+        }
+        else if (n3 >= 'A' && n3 <= 'F') {
+            n1 = n3 - 'A' + 10;
+        }
+        else if (n3 >= 'a' && n3 <= 'f') {
+            n1 = n3 - 'a' + 10;
+        }
+        ans += n1 * pow(16, n2 - i - 1);
     }
+    printf("%d", ans);
+}
 
-    else {
-      i = 1;
-      n1 = arr[i];
+void dth() {
+int n;
+    printf("Enter a decimal number: ");
+    scanf("%d", &n);
+    printf("Hexadecimal: %X ", n);
+
+    }
+    
+void bth() {
+int arr[20];
+int n;    
+
+int s = 0, sum = 0;
+int i, n1;
+
+printf("Enter number of bits: ");
+scanf("%d", &n);
+
+
+
+for (i = 0; i < n; i++) {
+    printf("Bit %d: ", i + 1);
+    scanf("%d", &arr[i]);
+    if (arr[i] < 0 || arr[i] > 1) {
+        printf("Enter a valid value\n");
+        i--;
+    }
+    
+}
+
+ 
+  for (i = 0; i < n; i++) {
+    
+    if ( arr[i] == 1) {
+      sum += pow(2, i);
       }
-     j += 1;
+  }
+  printf("Hexadecimal: %X ", sum);
 }
 
-for (i = j; i >= 0; i++) {
-  if (i == 1) {
-    sum += pow(2, j);
+void htb() {
+    
+char val[20];
+int i, n2, n1;
+int ans = 0;
+int arr [20];
+int k = 0;
+
+printf("Enter Hexadecimal number: ");
+scanf("%s", val);
+    
+  n2 = strlen(val);
+  
+    
+    for (i = 0; i < n2; i++) {
+        char n3 = val[i];
+        
+        if (n3 >= '0' && n3 <= '9') {
+            n1 = n3 - '0';
+        }
+        else if (n3 >= 'A' && n3 <= 'F') {
+            n1 = n3 - 'A' + 10;
+        }
+        else if (n3 >= 'a' && n3 <= 'f') {
+            n1 = n3 - 'a' + 10;
+        }
+        ans += n1 * pow(16, n2 - i - 1);
     }
+
+
+    while (ans > 0) {
+        arr[k] = ans % 2; 
+        ans /= 2;         
+        k++;
+    }
+
+    printf("Hexadecimal to binary: ");
+    for (int j = k - 1; j >= 0; j--) {  
+        printf("%d", arr[j]);
+    }
+    printf("\n");
 }
+
+    
 
 
 int main() {
-int arr[20], n;
+    int a;
 
-btd(arr, n);
+printf("Enter 1 to convert binary to decimal\n2 for decimal to binary\n3 for hexadecimal to decimal\n4 for decimal to hexadecimal\n5 for binary to hexadecimal\n6 for hexadecimal to binary : ");
+scanf("%d", &a);
+
+if (a == 1) {
+    btd();
+}
+
+if (a == 2) {
+    dtb();
+}
+
+if (a == 3) {
+    htd();
+}
+
+if (a == 4) {
+    dth();
+}
+
+if (a == 5) {
+    bth();
+}
+
+if (a == 6) {
+    htb();
+}
 return 0;
-
 }
